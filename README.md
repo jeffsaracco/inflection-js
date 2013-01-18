4f53a41809ca8c8436bab925e280de2196f4fbcb
Jeff Saracco <jeffsaracco@gmail.com>
2013-01-17 20:23:38 -0500

    Write Jasmine tests and fix all broken functions

--- README.md
+++ README.md
@@ -2,3 +2,61 @@ inflection-js
 =============
 
 JavaScript Inflection Support
+
+This code is based in part on the work done in Ruby to support
+infection as part of Ruby on Rails in the ActiveSupport's Inflector
+and Inflections classes.  It was initally ported to Javascript by
+Ryan Schuft (ryan.schuft@gmail.com) in 2007.
+
+The basic usage is:
+  1. Include this script on your web page.
+  2. Call functions on any InflectionJS object in Javascript
+
+Currently implemented functions:
+
+  InflectionJS.pluralize(plural)
+    renders a singular English language noun into its plural form
+    normal results can be overridden by passing in an alternative
+
+  InflectionJS.singularize(singular)
+    renders a plural English language noun into its singular form
+    normal results can be overridden by passing in an alterative
+
+  InflectionJS.camelize(lowFirstLetter)
+    renders a lower case underscored word into camel case
+    the first letter of the result will be upper case unless you pass true
+    also translates "/" into "::" (underscore does the opposite)
+
+  InflectionJS.underscore()
+    renders a camel cased word into words seperated by underscores
+    also translates "::" back into "/" (camelize does the opposite)
+
+  InflectionJS.humanize(lowFirstLetter)
+    renders a lower case and underscored word into human readable form
+    defaults to making the first letter capitalized unless you pass true
+
+  InflectionJS.capitalize()
+    renders all characters to lower case and then makes the first upper
+
+  InflectionJS.dasherize()
+    renders all underbars and spaces as dashes
+
+  InflectionJS.titleize()
+    renders words into title casing (as for book titles)
+
+  InflectionJS.demodulize()
+    renders class names that are prepended by modules into just the class
+
+  InflectionJS.tableize()
+    renders camel cased singular words into their underscored plural form
+
+  InflectionJS.classify()
+    renders an underscored plural word into its camel cased singular form
+
+  InflectionJS.foreign\_key(dropIdUbar)
+    renders a class name (camel cased singular noun) into a foreign key
+    defaults to seperating the class from the id with an underbar unless
+    you pass true
+
+  InflectionJS.ordinalize()
+    renders all numbers found in the string into their sequence like "22nd"
---
+++ spec/inflection_spec.js
@@ -0,0 +1,87 @@
+describe("InflectionJS", function() {
+  describe("#pluralize", function() {
+    it("pluralizes the words", function() {
+      expect(InflectionJS.pluralize("person")).toEqual("people");
+      expect(InflectionJS.pluralize("octopus")).toEqual("octopi");
+      expect(InflectionJS.pluralize("Hat")).toEqual("Hats");
+      expect(InflectionJS.pluralize("guy")).toEqual("guys");
+    });
+  });
+
+  describe("#singularize", function() {
+    it("signularizes the words", function() {
+      expect(InflectionJS.singularize("people")).toEqual("person");
+      expect(InflectionJS.singularize("octopi")).toEqual("octopus");
+      expect(InflectionJS.singularize("Hats")).toEqual("Hat");
+      expect(InflectionJS.singularize("guys")).toEqual("guy");
+    });
+  });
+
+  describe("#camelize", function() {
+    it("camelizes the words", function() {
+      expect(InflectionJS.camelize("message_properties")).toEqual("MessageProperties");
+      expect(InflectionJS.camelize("message_properties", true)).toEqual("messageProperties");
+    });
+  });
+
+  describe("#underscore", function() {
+    it("underscores the words", function() {
+      expect(InflectionJS.underscore("MessageProperties")).toEqual("message_properties");
+      expect(InflectionJS.underscore("messageProperties")).toEqual("message_properties");
+    });
+  });
+
+  describe("#humanize", function() {
+    it("humanizes the words", function() {
+      expect(InflectionJS.humanize("message_properties")).toEqual("Message properties");
+      expect(InflectionJS.humanize("message_properties", true)).toEqual("message properties");
+    });
+  });
+
+  describe("#capitalize", function() {
+    it("capitalizes the words", function() {
+      expect(InflectionJS.capitalize("message_properties")).toEqual("Message_properties");
+    });
+  });
+
+  describe("#dasherize", function() {
+    it("dasherizes the words", function() {
+      expect(InflectionJS.dasherize("message_properties")).toEqual("message-properties");
+      expect(InflectionJS.dasherize("Message Properties")).toEqual("Message-Properties");
+    });
+  });
+
+  describe("#titleize", function() {
+    it("titleizes the words", function() {
+      expect(InflectionJS.titleize("message_properties")).toEqual("Message Properties");
+      expect(InflectionJS.titleize("message properties to keep")).toEqual("Message Properties To Keep");
+    });
+  });
+
+  describe("#demodulize", function() {
+    it("demodulizes the words", function() {
+      expect(InflectionJS.demodulize("Message::Bus::Properties")).toEqual("Properties");
+    });
+  });
+
+  describe("#tableize", function() {
+    it("tableizes the words", function() {
+      expect(InflectionJS.tableize("MessageBusProperty")).toEqual("message_bus_properties");
+    });
+  });
+
+  describe("#classify", function() {
+    it("classifies the words", function() {
+      expect(InflectionJS.classify("message_bus_properties")).toEqual("MessageBusProperty");
+    });
+  });
+
+  describe("#foreign_key", function() {
+    it("converts teh word to a foreign key", function() {
+      expect(InflectionJS.foreign_key("MessageBusProperty")).toEqual("message_bus_property_id");
+      expect(InflectionJS.foreign_key("MessageBusProperty", true)).toEqual("message_bus_propertyid");
+    });
+  });
+});
+
+
